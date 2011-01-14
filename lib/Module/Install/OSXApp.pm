170088727dc44113355fccdb4bb9aec1aba121c1
Kazuhiro Osawa <yappo@shibuya.pl>
2011-01-14 19:05:16 +0900

    create prototype

--- lib/Module/Install/OSXApp.pm
+++ lib/Module/Install/OSXApp.pm
@@ -1,7 +1,53 @@
 package Module::Install::OSXApp;
 use strict;
 use warnings;
+use base qw(Module::Install::Base);
 our $VERSION = '0.01';
+
+sub osx_app {
+    my($self, %args) = @_;
+    my $name   = $args{name};
+    my $script = $args{script};
+    my $info_plist = _gen_info_plist(%args);
+
+    open my $fh, '>', 'inc/Info.plist';
+    print $fh $info_plist;
+    close $fh;
+
+    $self->Makefile->postamble(<<EOM);
+osxapp: 
+\t\$(NOECHO) rm -fr $name.app
+\t\$(NOECHO) mkdir -p $name.app/Contents
+\t\$(NOECHO) mkdir $name.app/Contents/MacOS
+\t\$(NOECHO) mkdir $name.app/Contents/Resources
+\t\$(NOECHO) cp $script $name.app/Contents/MacOS/$script
+\t\$(NOECHO) chmod 0755 $name.app/Contents/MacOS/$script
+\t\$(NOECHO) cp inc/Info.plist $name.app/Contents/Info.plist
+EOM
+}
+
+sub _gen_info_plist {
+    my %args = @_;
+    my $name   = $args{name};
+    my $script = $args{script};
+
+    return <<EOP;
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>CFBundleExecutable</key>
+    <string>$script</string>
+    <key>CFBundleIconFile</key>
+    <string>icon.icns</string>
+    <key>CFBundlePackageType</key>
+    <string>APPL</string>
+    <key>CFBundleSignature</key>
+    <string>????</string>
+  </dict>
+</plist>
+EOP
+}
 
 1;
 __END__
